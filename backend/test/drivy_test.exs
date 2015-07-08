c504afa63866137056754c4e85f1ba280fd37099
Howard Wilson <howard@watsonbox.net>
2015-07-08 15:10:53 +0200

    Level 2 implementation

--- backend/test/drivy_test.exs
+++ backend/test/drivy_test.exs
@@ -6,4 +6,10 @@ defmodule DrivyTest do
     json = File.read!("level1/output.json") |> String.split |> Enum.join
     assert Drivy.Level1.transform("level1/data.json") == json
   end
+
+  test "Level 2" do
+    # Get result JSON with whitespace removed
+    json = File.read!("level2/output.json") |> String.split |> Enum.join
+    assert Drivy.Level2.transform("level2/data.json") == json
+  end
 end
