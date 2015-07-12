4f3931bac32be3fef458b449e678adda47077a1e
Howard Wilson <howard@watsonbox.net>
2015-07-13 00:33:57 +0200

    Assorted re-factoring

--- backend/lib/level6.ex
+++ backend/lib/level6.ex
@@ -1,36 +1,42 @@
 defmodule Drivy.Level6 do
   use Timex
 
+  @date_format "{YYYY}-{0M}-{0D}"
+
   def transform(path) do
-    data = JSON.decode!(File.read!(path))
+    data = path |> File.read! |> JSON.decode!
 
     rentals = data["rentals"]
-    |> rentals_with_cars(data["cars"])
-    |> rentals_with_durations
-    |> rentals_with_prices
+    |> add_by_id(:car, data["cars"]) # Add cars to rental data
+    |> add_rentals_prices
 
     data["rental_modifications"]
-    |> rental_mods_with_rentals(rentals)
+    |> add_by_id(:rental, rentals) # Add rentals to rental modification data
     |> rental_mods_data
     |> JSON.encode!
   end
 
-  def rentals_with_cars(rentals, cars) do
-    cars_by_id = index_by_id(cars)
-    for rental <- rentals, do: Map.put(rental, :car, cars_by_id[rental["car_id"]])
+  def add_by_id(target_collection, field, source_collection) do
+    collection_by_id = source_collection |> index_by("id")
+
+    for x <- target_collection do
+      Map.put(x, field, collection_by_id[x[Atom.to_string(field) <> "_id"]])
+    end
   end
 
-  def rentals_with_durations(rentals) do
-    for rental <- rentals, do: Map.put(rental, :duration, rental_duration(rental))
+  def add_rentals_prices(rentals) do
+    Enum.map rentals, &add_rental_price/1
   end
 
-  def rentals_with_prices(rentals) do
-    for rental <- rentals, do: Map.put(rental, :price, rental_price(rental))
+  def add_rental_price(rental) do
+    rental
+    |> Map.put(:duration, rental_duration(rental))
+    |> (fn(r) -> Map.put(r, :price, rental_price(r)) end).()
   end
 
-  def rental_mods_with_rentals(rental_mods, rentals) do
-    rentals_by_id = index_by_id(rentals)
-    for rental_mod <- rental_mods, do: Map.put(rental_mod, :rental, rentals_by_id[rental_mod["rental_id"]])
+  def add_rental_mods_rentals(rental_mods, rentals) do
+    rentals_by_id = rentals |> index_by("id")
+    for rm <- rental_mods, do: Map.put(rm, :rental, rentals_by_id[rm["rental_id"]])
   end
 
   # Presenter for rental modifications
@@ -50,15 +56,13 @@ defmodule Drivy.Level6 do
 
   # Actions for a modification are the difference between those for the rental pre and post modification
   def rental_actions(rental_mod = %{rental: rental}) do
-    diff_rental_actions(
-      rental_mod
-        |> Map.take(["distance", "start_date", "end_date"])
-        |> Map.to_list
-        |> apply_rental_mods(rental)
-        |> rental_actions,
-      rental_mod.rental
-        |> rental_actions
-    )
+    actions_after_mod = rental_mod
+    |> Map.take(["distance", "start_date", "end_date"])
+    |> Map.to_list
+    |> apply_rental_mods(rental)
+    |> rental_actions
+
+    diff_rental_actions(actions_after_mod, rental_mod.rental |> rental_actions)
   end
 
   # Internal representation of actions for a given rental
@@ -67,7 +71,7 @@ defmodule Drivy.Level6 do
 
     [
       driver: -(rental.price + rental_deductible_reduction(rental)),
-      owner: rental.price - rental_commission_total(rental),
+      owner: rental.price - (commission |> Map.values |> Enum.sum),
       insurance: commission.insurance_fee,
       assistance: commission.assistance_fee,
       drivy: commission.drivy_fee + rental_deductible_reduction(rental)
@@ -79,14 +83,9 @@ defmodule Drivy.Level6 do
     for { actor, amount } <- action1, into: [], do: { actor, amount - action2[actor] }
   end
 
+  def apply_rental_mods([], rental), do: add_rental_price(rental)
   def apply_rental_mods([{k, v} | rest], rental) do
     rest |> apply_rental_mods(Map.put(rental, k, v))
-  end
-
-  def apply_rental_mods([], rental) do
-    rental
-    |> Map.put(:duration, rental_duration(rental))
-    |> Map.put(:price, rental_price(rental))
   end
 
   def rental_price(rental) do
@@ -94,12 +93,9 @@ defmodule Drivy.Level6 do
   end
 
   def duration_price(rental) do
-    price_per_day = rental.car["price_per_day"]
-
-    rental
-    |> rental_duration
+    rental.duration
     |> discounts
-    |> Enum.map(&apply_discount(price_per_day, &1))
+    |> Enum.map(&apply_discount(rental.car["price_per_day"], &1))
     |> Enum.sum
     |> round
   end
@@ -109,8 +105,8 @@ defmodule Drivy.Level6 do
   end
 
   def rental_duration(rental) do
-    start_date = DateFormat.parse!(rental["start_date"], "{YYYY}-{0M}-{0D}")
-    end_date = DateFormat.parse!(rental["end_date"], "{YYYY}-{0M}-{0D}")
+    start_date = rental["start_date"] |> DateFormat.parse!(@date_format)
+    end_date = rental["end_date"] |> DateFormat.parse!(@date_format)
     Date.diff(start_date, end_date, :days) + 1
   end
 
@@ -129,7 +125,7 @@ defmodule Drivy.Level6 do
   def discount(_), do: 0.5
 
   def rental_commission(rental) do
-    commission = rental_commission_total(rental)
+    commission = rental.price * 0.3
 
     %{
       insurance_fee: round(commission / 2),
@@ -138,20 +134,14 @@ defmodule Drivy.Level6 do
     }
   end
 
-  def rental_commission_total(rental) do
-    rental.price * 0.3
-  end
-
   def rental_deductible_reduction(%{"deductible_reduction" => false}), do: 0
-  def rental_deductible_reduction(rental) do
-    rental.duration * 400
-  end
+  def rental_deductible_reduction(rental), do: rental.duration * 400
 
   defp transform_values(dict, transform) do
     for { k, v } <- dict, into: [], do: { k, transform.(v) }
   end
 
-  defp index_by_id(array) do
-    for x <- array, into: %{}, do: { x["id"], x }
+  defp index_by(list, field) do
+    for x <- list, into: %{}, do: { x[field], x }
   end
 end
